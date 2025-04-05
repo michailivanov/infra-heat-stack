a850726f096da02386864d795d0959d6e9226872
michailivanov <85007060+michailivanov@users.noreply.github.com>
2025-04-05 13:09:25 +0300

    Create variables.tf

---
+++ tf-yandex/variables.tf
@@ -0,0 +1,14 @@
+variable "yc_token" {
+  description = "Yandex Cloud API token"
+  type        = string
+}
+
+variable "yc_cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+}
+
+variable "yc_folder_id" {
+  description = "Yandex Cloud folder ID"
+  type        = string
+}
